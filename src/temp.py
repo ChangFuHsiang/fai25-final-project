2a6355fdc495d1088586c6ae4b79ffb16b8f9e98
ChangFuHsiang <shine0921873532@gmail.com>
2025-06-15 00:34:56 +0900

    18

--- src/temp.py
+++ src/temp.py
@@ -45,8 +45,13 @@ class MonteCarloPlayer(BasePokerPlayer):
                 return 'raise', min(2 * min_raise, my_stack)
             elif win_rate >= 0.55:
                 return 'call', call_money
-            elif win_rate >= 0.47:
+            elif win_rate >= 0.5:
                 if risk_ratio < 0.2:
+                    return 'call', call_money
+                else:
+                    return 'fold', 0
+            elif win_rate >= 0.4:
+                if call_money <= 20:
                     return 'call', call_money
                 else:
                     return 'fold', 0
